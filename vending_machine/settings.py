b2fa31551fc508042a6e68201e436862040c32d5
Sergi Ortega M <sergi@abacum.io>
2023-07-28 15:56:40 +0200

    wip

--- vending_machine/settings.py
+++ vending_machine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qc@3y#z)!307s8p8kg-jnsf25!1(80zvk8(d!0cflto+ml^_zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -69,8 +69,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vending_machine.wsgi.application'
-
+# WSGI_APPLICATION = 'vending_machine.wsgi.application'
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
